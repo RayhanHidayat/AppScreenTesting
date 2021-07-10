03fffdb15c5e78f47fc237ba7eb2a7ab2feb50d8
rayhan_hidayat <rayhananh2001@gmail.com>
2021-07-10 20:59:26 +0700

    finishing on guest recycler view

--- app/src/main/java/com/rayhan/apptestscreening/ui/guest/GuideActivity.kt
+++ app/src/main/java/com/rayhan/apptestscreening/ui/guest/GuideActivity.kt
@@ -1,16 +1,34 @@
 package com.rayhan.apptestscreening.ui.guest
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.rayhan.apptestscreening.databinding.ActivityGuideBinding
 
 class GuideActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGuideBinding
+    private val guestViewModel by viewModels<GuestViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGuideBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        title = "Guest"
+
+        guestViewModel.getGuideData()
+
+        guestViewModel.guestData.observe(this, {
+            renderAllData(it)
+        })
+    }
+
+    private fun renderAllData(list: List<GuideModel>) {
+        binding.run {
+            rvEvent.layoutManager = GridLayoutManager(this@GuideActivity, 2)
+            val adapter = GuestAdapter(list.toMutableList())
+            rvEvent.adapter = adapter
+        }
     }
 }
